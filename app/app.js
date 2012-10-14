92e8f51fffa6eb7f33a67bd18aff0b10fd39c2fa
Alex Antipin <alex@labs42.com>
2012-10-14 15:46:47 +0400

    Project sceleton structure created

---
+++ app/app.js
@@ -0,0 +1,17 @@
+define(function(require){
+
+    var
+        $ = require('jquery'),
+        $ = require('bootstrap'),
+        _ = require('lodash'),
+        Backbone = require('backbone');
+
+
+    return {
+        initialize : function() {
+            console.log('App inited');
+            console.log(App);
+        }
+    }
+
+});
---
+++ app/main.js
@@ -0,0 +1,72 @@
+/* ---------------------------------------- Helpers ---------------------------------------- */
+if (typeof console == "undefined") { window.console = { log: function () {} }; }
+
+function buildScriptPath(path, version) {
+    var
+        chunk_path = path,
+        chunk_version = version != undefined ? "-" + version : "",
+        path = chunk_path + chunk_version;
+
+    return App.settings.debug ? path + ".min" : path;
+}
+
+
+/* ---------------------------------------- App configuration ---------------------------------------- */
+var App = App || {};
+App.settings = App.settings || {};
+App.settings.debug = true;
+
+
+/* ---------------------------------------- Requirejs configuration ---------------------------------------- */
+require.config({
+
+    baseUrl: "/app",
+
+    paths: {
+
+        // Libs
+        "jquery":          buildScriptPath("../lib/jquery/jquery",           "1.8.2"),
+        "lodash":          buildScriptPath("../lib/backbone/lodash",         "0.8.2"),
+        "backbone":        buildScriptPath("../lib/backbone/backbone",       "0.9.2"),
+        "handlebars":      buildScriptPath("../lib/handlebars/handlebars",   "1.0.rc.1"),
+        "text":            buildScriptPath("../lib/require/text",            "2.0.3"),
+        "bootstrap":       buildScriptPath("../lib/bootstrap/js/bootstrap"),
+
+        // App
+        "app":             "app"
+    },
+
+    shim: {
+
+        "backbone": {
+            deps: ["lodash", "jquery" ],
+            exports: "Backbone"
+        },
+
+        "handlebars": {
+            exports: "Handlebars"
+        },
+
+        "lodash": {
+            exports: "_"
+        },
+
+        "bootstrap": {
+            deps: ['jquery'],
+            exports: "$"
+        }
+
+    }
+
+});
+
+
+
+/* ---------------------------------------- App init ---------------------------------------- */
+require(["jquery", "lodash", "app"],
+    function( $, _, app) {
+        $(function(){
+            App = _.extend(app, App);
+            App.initialize();
+        });
+    });
